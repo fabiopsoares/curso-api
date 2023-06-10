81f135f9ed8c5991c3697736a7e6634a3e96afca
fabiopsoares <fabiotecninf@gmail.com>
2023-06-10 09:13:58 -0300

    user classe usuário

---
+++ src/main/java/br/com/cursoapi/api/domain/User.java
@@ -0,0 +1,25 @@
+package br.com.cursoapi.api.domain;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+@ToString
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+    @Column(unique = true)
+    private String email;
+    private String password;
+
+
+}
