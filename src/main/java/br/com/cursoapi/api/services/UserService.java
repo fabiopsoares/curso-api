c9ab31bdaf5d1ff36ad24e119b4b1f25016d02d6
fabiopsoares <fabiotecninf@gmail.com>
2023-06-10 11:20:26 -0300

    endpoint findAll para user

--- src/main/java/br/com/cursoapi/api/services/UserService.java
+++ src/main/java/br/com/cursoapi/api/services/UserService.java
@@ -2,7 +2,11 @@ package br.com.cursoapi.api.services;
 
 import br.com.cursoapi.api.domain.User;
 
+import java.util.List;
+
 public interface UserService {
 
     User findById(Integer id);
+
+    List<User> findAll();
 }
