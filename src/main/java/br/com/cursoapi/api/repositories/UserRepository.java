887768c15af990fef85271a87d9eeedef65d305c
fabiopsoares <fabiotecninf@gmail.com>
2023-06-10 09:55:51 -0300

    interface user repository

---
+++ src/main/java/br/com/cursoapi/api/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package br.com.cursoapi.api.repositories;
+
+import br.com.cursoapi.api.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+}
