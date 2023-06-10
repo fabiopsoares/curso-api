c74f5f990fdf1ba83778d77e3a8a3ffe02fbfc9e
fabiopsoares <fabiotecninf@gmail.com>
2023-06-10 12:32:12 -0300

    exceção para e-mail já cadastrado

--- src/main/java/br/com/cursoapi/api/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/br/com/cursoapi/api/resources/exceptions/ResourceExceptionHandler.java
@@ -1,5 +1,6 @@
 package br.com.cursoapi.api.resources.exceptions;
 
+import br.com.cursoapi.api.services.exceptions.DataIntegratyViolationException;
 import br.com.cursoapi.api.services.exceptions.ObjectNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,5 +18,12 @@ public class ResourceExceptionHandler {
         StandardError error = new StandardError(LocalDateTime.now(), HttpStatus.NOT_FOUND.value(), ex.getMessage(), request.getRequestURI());
 
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+    }
+
+    @ExceptionHandler(DataIntegratyViolationException.class)
+    public ResponseEntity<StandardError> objectNotFound(DataIntegratyViolationException ex, HttpServletRequest request) {
+        StandardError error = new StandardError(LocalDateTime.now(), HttpStatus.BAD_REQUEST.value(), ex.getMessage(), request.getRequestURI());
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
     }
 }
--- src/main/java/br/com/cursoapi/api/services/impl/UserServiceImpl.java
+++ src/main/java/br/com/cursoapi/api/services/impl/UserServiceImpl.java
@@ -4,6 +4,7 @@ import br.com.cursoapi.api.domain.User;
 import br.com.cursoapi.api.domain.dto.UserDTO;
 import br.com.cursoapi.api.repositories.UserRepository;
 import br.com.cursoapi.api.services.UserService;
+import br.com.cursoapi.api.services.exceptions.DataIntegratyViolationException;
 import br.com.cursoapi.api.services.exceptions.ObjectNotFoundException;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -32,6 +33,14 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public User create(UserDTO obj) {
+        findByEmail(obj);
         return repository.save(mapper.map(obj, User.class));
+    }
+
+    private void findByEmail(UserDTO obj){
+        Optional<User> user = repository.findByEmail(obj.getEmail());
+        if(user.isPresent()){
+            throw new DataIntegratyViolationException("E-mail já cadastrado no sistema");
+        }
     }
 }
